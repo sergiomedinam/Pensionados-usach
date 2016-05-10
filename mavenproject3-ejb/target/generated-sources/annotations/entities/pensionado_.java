f10be1a842f8be54735ca8e9aacd2201c31648a2
Sergio <medina159@gmail.com>
2016-05-09 23:51:46 -0300

    validaciones de pensionados y usuarios

--- mavenproject3-ejb/target/generated-sources/annotations/entities/pensionado_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/pensionado_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-09T22:29:47")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-09T23:39:17")
 @StaticMetamodel(pensionado.class)
 public class pensionado_ { 
 
