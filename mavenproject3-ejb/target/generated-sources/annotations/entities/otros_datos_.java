8a4f477c338e0c1b07716d054dcd0a3057f6f57f
Sergio <medina159@gmail.com>
2016-05-11 16:35:11 -0300

    Mejoras en barra de navegacion

--- mavenproject3-ejb/target/generated-sources/annotations/entities/otros_datos_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/otros_datos_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-10T22:31:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-11T15:47:03")
 @StaticMetamodel(otros_datos.class)
 public class otros_datos_ { 
 
--- mavenproject3-ejb/target/generated-sources/annotations/entities/pensionado_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/pensionado_.java
@@ -11,7 +11,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-10T22:31:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-11T15:47:03")
 @StaticMetamodel(pensionado.class)
 public class pensionado_ { 
 
@@ -19,6 +19,7 @@ public class pensionado_ {
     public static volatile SingularAttribute<pensionado, patologia> patologias;
     public static volatile SingularAttribute<pensionado, Long> telefono_fijo;
     public static volatile SingularAttribute<pensionado, String> rut_pensionado;
+    public static volatile SingularAttribute<pensionado, String> email_alternativo;
     public static volatile SingularAttribute<pensionado, String> apellido_p_pensionado;
     public static volatile SingularAttribute<pensionado, String> fecha_nacimiento;
     public static volatile SingularAttribute<pensionado, String> estado_civil;
@@ -28,6 +29,7 @@ public class pensionado_ {
     public static volatile SingularAttribute<pensionado, medicamento> medicamentos;
     public static volatile SingularAttribute<pensionado, String> comuna;
     public static volatile SingularAttribute<pensionado, Long> celular;
+    public static volatile SingularAttribute<pensionado, String> email_pensionado;
     public static volatile SingularAttribute<pensionado, contacto> contactos;
     public static volatile SingularAttribute<pensionado, Long> id;
     public static volatile SingularAttribute<pensionado, String> region;
