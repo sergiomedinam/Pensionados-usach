cc663ff9f38f008e2a049f3f766616a5ac942793
Nelson Jerez <nelson.jerezv@gmail.com>
2016-05-12 01:10:10 -0300

    eliminado un tipo de usuario que no tenia sentido

--- mavenproject3-ejb/target/generated-sources/annotations/entities/medicamento_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/medicamento_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-12T00:52:26")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-12T01:06:09")
 @StaticMetamodel(medicamento.class)
 public class medicamento_ { 
 
