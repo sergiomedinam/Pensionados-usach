2cb3fe87eb865bb596b1c3ffa5cd3b77080a836d
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2016-05-12 16:44:36 -0300

    xhtml mapa

--- mavenproject3-ejb/target/generated-sources/annotations/entities/contacto_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/contacto_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-12T12:37:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-12T16:41:42")
 @StaticMetamodel(contacto.class)
 public class contacto_ { 
 
