0081043ebb19242e626ada8ffc4e16f66a6e7c6c
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2016-05-12 23:42:51 -0300

    Filtros Comuna y Región
    Se añade "FiltrosLocalizacion.xhtml" en donde se implementan filtros
    sobre las comunas y regiones

--- mavenproject3-ejb/target/generated-sources/annotations/entities/rol_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/rol_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-12T16:41:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-12T22:23:00")
 @StaticMetamodel(rol.class)
 public class rol_ { 
 
