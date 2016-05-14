850c801b1742d6657bc8c861c94a7a3a01293682
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2016-05-14 02:59:45 -0300

    Agregadas regiones

--- mavenproject3-ejb/target/generated-sources/annotations/entities/medicamento_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/medicamento_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-13T19:29:28")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T02:52:20")
 @StaticMetamodel(medicamento.class)
 public class medicamento_ { 
 
