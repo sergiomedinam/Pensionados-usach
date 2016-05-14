ce187e0078500eba0da051951ea562514e140bf3
Sergio <medina159@gmail.com>
2016-05-14 17:04:26 -0300

    Mejorado Template de Login.

--- mavenproject3-ejb/target/generated-sources/annotations/entities/otros_datos_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/otros_datos_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T03:49:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T16:47:29")
 @StaticMetamodel(otros_datos.class)
 public class otros_datos_ { 
 
