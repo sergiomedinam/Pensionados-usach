8aac8de44ed65a4dcb10c34b2283805e6b563384
Sergio <medina159@gmail.com>
2016-05-14 18:49:51 -0300

    Mejorado pequeño en template de index y barra navegacion para el login

--- mavenproject3-ejb/target/generated-sources/annotations/entities/beneficio_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/beneficio_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T17:23:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T18:47:00")
 @StaticMetamodel(beneficio.class)
 public class beneficio_ { 
 
