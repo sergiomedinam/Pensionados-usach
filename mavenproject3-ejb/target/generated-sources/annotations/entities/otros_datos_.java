ca0d75ba951ddf803d9c06c429f41e238c2dfb24
Sergio <medina159@gmail.com>
2016-05-13 23:51:30 -0300

    arreglo de Template para seguridad de usuarios.

--- mavenproject3-ejb/target/generated-sources/annotations/entities/otros_datos_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/otros_datos_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-13T22:05:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-13T23:48:29")
 @StaticMetamodel(otros_datos.class)
 public class otros_datos_ { 
 
