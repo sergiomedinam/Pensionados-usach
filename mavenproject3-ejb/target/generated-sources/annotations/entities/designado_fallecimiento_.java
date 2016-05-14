b8353d48dfda30750a55ed4d7ece72bc3651afeb
Nelson Jerez <nelson.jerezv@gmail.com>
2016-05-14 04:03:09 -0300

    cambio de algunas rutas fijas a relativas, mapeo de seguridad desde la raiz, comentado por el momento

--- mavenproject3-ejb/target/generated-sources/annotations/entities/designado_fallecimiento_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/designado_fallecimiento_.java
@@ -5,11 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T00:53:22")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-13T23:48:29")
->>>>>>> refs/remotes/origin/master
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T03:49:49")
 @StaticMetamodel(designado_fallecimiento.class)
 public class designado_fallecimiento_ { 
 
--- mavenproject3-ejb/target/generated-sources/annotations/entities/tipo_beneficio_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/tipo_beneficio_.java
@@ -4,11 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T00:53:22")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-13T23:48:29")
->>>>>>> refs/remotes/origin/master
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T03:49:49")
 @StaticMetamodel(tipo_beneficio.class)
 public class tipo_beneficio_ { 
 
