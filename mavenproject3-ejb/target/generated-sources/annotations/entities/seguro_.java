44027abc0a8b792f0f25461b161f34a31e2c5c67
Sergio <medina159@gmail.com>
2016-05-15 17:23:20 -0400

    Se deja el Template definitivo.

--- mavenproject3-ejb/target/generated-sources/annotations/entities/seguro_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/seguro_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-14T22:17:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-15T16:48:47")
 @StaticMetamodel(seguro.class)
 public class seguro_ { 
 
