cc5c182c95152841ea43b4d6c810eca677ef2c55
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2016-05-22 19:29:38 -0400

    Bug comuna (EDIT) + mascaras en formularios

--- mavenproject3-ejb/target/generated-sources/annotations/entities/rol_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/rol_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-22T01:24:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-22T18:39:27")
 @StaticMetamodel(rol.class)
 public class rol_ { 
 
