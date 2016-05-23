a2a786e8e122e487da96bd9c4b1edc0d70929189
Sergio <medina159@gmail.com>
2016-05-22 23:00:15 -0400

    Cambio en Primefaces - Confirmacion al eliminar usuario

--- mavenproject3-ejb/target/generated-sources/annotations/entities/otros_datos_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/otros_datos_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-22T18:39:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-22T23:58:28")
 @StaticMetamodel(otros_datos.class)
 public class otros_datos_ { 
 
