5e83091621a532728030d8bdcd8bd4d120c61744
Sergio Medina <medina159@gmail.com>
2016-05-24 10:19:02 -0400

    arreglo de vista de permisos denegados

--- mavenproject3-ejb/target/generated-sources/annotations/entities/pensionado_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/pensionado_.java
@@ -12,31 +12,31 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-23T18:46:21")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-24T09:56:53")
 @StaticMetamodel(pensionado.class)
 public class pensionado_ { 
 
-    public static volatile SingularAttribute<pensionado, String> apellido_m_pensionado;
-    public static volatile SingularAttribute<pensionado, patologia> patologias;
-    public static volatile SingularAttribute<pensionado, Long> telefono_fijo;
-    public static volatile SingularAttribute<pensionado, String> rut_pensionado;
-    public static volatile SingularAttribute<pensionado, String> email_alternativo;
-    public static volatile SingularAttribute<pensionado, String> apellido_p_pensionado;
-    public static volatile SingularAttribute<pensionado, String> fecha_nacimiento;
-    public static volatile SingularAttribute<pensionado, String> estado_civil;
-    public static volatile SingularAttribute<pensionado, String> direccion;
-    public static volatile SingularAttribute<pensionado, otros_datos> otros_datos;
-    public static volatile SingularAttribute<pensionado, Date> fnacimiento;
-    public static volatile SingularAttribute<pensionado, prestamo> prestamos;
-    public static volatile SingularAttribute<pensionado, medicamento> medicamentos;
-    public static volatile SingularAttribute<pensionado, String> comuna;
-    public static volatile SingularAttribute<pensionado, Long> celular;
-    public static volatile SingularAttribute<pensionado, String> email_pensionado;
-    public static volatile SingularAttribute<pensionado, contacto> contactos;
-    public static volatile SingularAttribute<pensionado, Long> id;
     public static volatile SingularAttribute<pensionado, String> region;
+    public static volatile SingularAttribute<pensionado, String> direccion;
+    public static volatile SingularAttribute<pensionado, String> estado_civil;
+    public static volatile SingularAttribute<pensionado, contacto> contactos;
+    public static volatile SingularAttribute<pensionado, String> email_pensionado;
     public static volatile SingularAttribute<pensionado, String> nombre_pensionado;
+    public static volatile SingularAttribute<pensionado, medicamento> medicamentos;
+    public static volatile SingularAttribute<pensionado, String> fecha_nacimiento;
+    public static volatile SingularAttribute<pensionado, Long> id;
+    public static volatile SingularAttribute<pensionado, String> apellido_p_pensionado;
+    public static volatile SingularAttribute<pensionado, String> comuna;
+    public static volatile SingularAttribute<pensionado, Date> fnacimiento;
+    public static volatile SingularAttribute<pensionado, patologia> patologias;
+    public static volatile SingularAttribute<pensionado, String> rut_pensionado;
+    public static volatile SingularAttribute<pensionado, prestamo> prestamos;
+    public static volatile SingularAttribute<pensionado, String> apellido_m_pensionado;
     public static volatile SingularAttribute<pensionado, seguro> seguros;
+    public static volatile SingularAttribute<pensionado, String> email_alternativo;
+    public static volatile SingularAttribute<pensionado, Long> telefono_fijo;
+    public static volatile SingularAttribute<pensionado, Long> celular;
     public static volatile SingularAttribute<pensionado, beneficio> beneficios;
+    public static volatile SingularAttribute<pensionado, otros_datos> otros_datos;
 
 }
--- mavenproject3-ejb/target/generated-sources/annotations/entities/tipo_prestamo_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/tipo_prestamo_.java
@@ -4,11 +4,11 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-23T18:46:21")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-24T09:56:53")
 @StaticMetamodel(tipo_prestamo.class)
 public class tipo_prestamo_ { 
 
-    public static volatile SingularAttribute<tipo_prestamo, String> nombre_t_prestamo;
     public static volatile SingularAttribute<tipo_prestamo, Long> id;
+    public static volatile SingularAttribute<tipo_prestamo, String> nombre_t_prestamo;
 
 }
