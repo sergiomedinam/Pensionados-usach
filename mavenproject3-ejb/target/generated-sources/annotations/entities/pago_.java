4fe593dacb86d1f0e021442dc0a14a1737c915ef
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2016-05-26 02:44:28 -0400

    Pequeña modificación template

--- mavenproject3-ejb/target/generated-sources/annotations/entities/pago_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/pago_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-26T02:11:59")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-26T02:06:36")
 @StaticMetamodel(pago.class)
 public class pago_ { 
 
--- mavenproject3-ejb/target/generated-sources/annotations/entities/prestamo_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/prestamo_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-26T02:11:59")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-26T02:06:36")
 @StaticMetamodel(prestamo.class)
 public class prestamo_ { 
 
