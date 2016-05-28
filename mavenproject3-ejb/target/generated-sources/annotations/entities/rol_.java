770f5aa6302be51a8412c59536ca5e03a657d48f
Rodrigo Mendoza <rodrigo.mendoza@usach.cl>
2016-05-28 18:35:37 -0400

    Incidencia morosos - mismo año

--- mavenproject3-ejb/target/generated-sources/annotations/entities/rol_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/rol_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-28T16:43:02")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-28T18:13:19")
 @StaticMetamodel(rol.class)
 public class rol_ { 
 
