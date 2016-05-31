8a0caf2058f89f79d1ec2cb920939985896fb5f3
Sergio <medina159@gmail.com>
2016-05-30 21:19:29 -0400

    cambios en Infopensionado

--- mavenproject3-ejb/target/generated-sources/annotations/entities/rol_.java
+++ mavenproject3-ejb/target/generated-sources/annotations/entities/rol_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-29T22:23:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-30T21:48:25")
 @StaticMetamodel(rol.class)
 public class rol_ { 
 
